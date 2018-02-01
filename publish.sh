013e7366e724679e6c992c1a04bd51293923ac5e
José Vieira <josevieira.dev@gmail.com>
2018-02-01 10:23:40 -0300

    Adds  and gradle-git

--- publish.sh
+++ publish.sh
@@ -1,3 +1,9 @@
-echo -e "Running script..."
-./gradlew build
-./gradlew -PbintrayUser="${BINTRAY_USER}" -PbintrayKey="${BINTRAY_KEY}" build bintrayUpload
+if [ "$TRAVIS_TAG" == "" ]; then
+  echo -e 'Build Branch'
+  ./gradlew build
+elif [ "$TRAVIS_TAG" != "" ]; then
+  echo -e 'Build Branch for tag: Tag ['$TRAVIS_TAG']'
+  ./gradlew -PbintrayUser="${BINTRAY_USER}" -PbintrayKey="${BINTRAY_KEY}" build bintrayUpload
+else
+  echo -e 'WARN: Should not be here ./gradlew clean'
+fi
